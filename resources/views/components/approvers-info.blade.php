019aff71e4042d14caaf0c35773289f199471247
PiotrTajanowicz <PiotrTajanowicz@9.145.94.213>
2020-09-03 13:49:58 +0200

    upd

--- resources/views/components/approvers-info.blade.php
+++ resources/views/components/approvers-info.blade.php
@@ -1,45 +1,45 @@
 <td bgcolor="{{ $col1 }}">&nbsp;</td>
 <td align='center'>
-	@!empty($lvl1Line1)
+	@empty(!$lvl1Line1)
     	<p>{{ $lvl1Line1 }}</p>
     @endempty
-    @!empty($lvl1Line2)
+    @empty(!$lvl1Line2)
     	<p>{{ $lvl1Line2 }}</p>
     @endempty
-    @!empty($lvl1Line3)
+    @empty(!$lvl1Line3)
     	<p>{{ $lvl1Line3 }}</p>
     @endempty
-    @!empty($lvl1Line4)
+    @empty(!$lvl1Line4)
     	<p>{{ $lvl1Line4 }}</p>
     @endempty
 </td>
 <td bgcolor="{{ $col2 }}">&nbsp;</td>
 <td align='center'>
-    @!empty($lvl2Line1)
+    @empty(!$lvl2Line1)
     	<p>{{ $lvl2Line1 }}</p>
     @endempty
-    @!empty($lvl2Line2)
+    @empty(!$lvl2Line2)
     	<p>{{ $lvl2Line2 }}</p>
     @endempty
-    @!empty($lvl2Line3)
+    @empty(!$lvl2Line3)
     	<p>{{ $lvl2Line3 }}</p>
     @endempty
-    @!empty($lvl2Line4)
+    @empty(!$lvl2Line4)
     	<p>{{ $lvl2Line4 }}</p>
     @endempty
 </td>
 <td bgcolor="{{ $col3 }}">&nbsp;</td>
 <td align='center'>
-	@!empty($lvl3Line1)
+	@empty(!$lvl3Line1)
     	<p>{{ $lvl3Line1 }}</p>
     @endempty
-    @!empty($lvl3Line2)
+    @empty(!$lvl3Line2)
     	<p>{{ $lvl3Line2 }}</p>
     @endempty
-    @!empty($lvl3Line3)
+    @empty(!$lvl3Line3)
     	<p>{{ $lvl3Line3 }}</p>
     @endempty
-    @!empty($lvl3Line4)
+    @empty(!$lvl3Line4)
     	<p>{{ $lvl3Line4 }}</p>
     @endempty
 </td>
