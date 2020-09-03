c727d1a47f47ae18571cd35e746a2151d5e09dff
PiotrTajanowicz <PiotrTajanowicz@9.145.94.213>
2020-09-03 13:52:46 +0200

    upd

--- resources/views/components/approvers-info.blade.php
+++ resources/views/components/approvers-info.blade.php
@@ -1,46 +1,46 @@
 <td bgcolor="{{ $col1 }}">&nbsp;</td>
 <td align='center'>
 	@empty(!$lvl1Line1)
-    	<p>{{ $lvl1Line1 }}</p>
+    	<p class="ibm-padding-bottom-0">{{ $lvl1Line1 }}</p>
     @endempty
     @empty(!$lvl1Line2)
-    	<p>{{ $lvl1Line2 }}</p>
+    	<p class="ibm-padding-bottom-0">{{ $lvl1Line2 }}</p>
     @endempty
     @empty(!$lvl1Line3)
-    	<p>{{ $lvl1Line3 }}</p>
+    	<p class="ibm-padding-bottom-0">{{ $lvl1Line3 }}</p>
     @endempty
     @empty(!$lvl1Line4)
-    	<p>{{ $lvl1Line4 }}</p>
+    	<p class="ibm-padding-bottom-0">{{ $lvl1Line4 }}</p>
     @endempty
 </td>
 <td bgcolor="{{ $col2 }}">&nbsp;</td>
 <td align='center'>
     @empty(!$lvl2Line1)
-    	<p>{{ $lvl2Line1 }}</p>
+    	<p class="ibm-padding-bottom-0">{{ $lvl2Line1 }}</p>
     @endempty
     @empty(!$lvl2Line2)
-    	<p>{{ $lvl2Line2 }}</p>
+    	<p class="ibm-padding-bottom-0">{{ $lvl2Line2 }}</p>
     @endempty
     @empty(!$lvl2Line3)
-    	<p>{{ $lvl2Line3 }}</p>
+    	<p class="ibm-padding-bottom-0">{{ $lvl2Line3 }}</p>
     @endempty
     @empty(!$lvl2Line4)
-    	<p>{{ $lvl2Line4 }}</p>
+    	<p class="ibm-padding-bottom-0">{{ $lvl2Line4 }}</p>
     @endempty
 </td>
 <td bgcolor="{{ $col3 }}">&nbsp;</td>
 <td align='center'>
 	@empty(!$lvl3Line1)
-    	<p>{{ $lvl3Line1 }}</p>
+    	<p class="ibm-padding-bottom-0">{{ $lvl3Line1 }}</p>
     @endempty
     @empty(!$lvl3Line2)
-    	<p>{{ $lvl3Line2 }}</p>
+    	<p class="ibm-padding-bottom-0">{{ $lvl3Line2 }}</p>
     @endempty
     @empty(!$lvl3Line3)
-    	<p>{{ $lvl3Line3 }}</p>
+    	<p class="ibm-padding-bottom-0">{{ $lvl3Line3 }}</p>
     @endempty
     @empty(!$lvl3Line4)
-    	<p>{{ $lvl3Line4 }}</p>
+    	<p class="ibm-padding-bottom-0">{{ $lvl3Line4 }}</p>
     @endempty
 </td>
 
