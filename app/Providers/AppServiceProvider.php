eae8a271c44efacc9880f9394e2bec26dc3a06f8
PiotrTajanowicz <PiotrTajanowicz@9.145.94.213>
2020-09-07 11:45:10 +0200

    upd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use App\View\Components\IbmV18Form\Input;
 use App\View\Components\IbmV18Form\Select;
 use App\View\Components\IbmV18Form\Textarea;
 use Illuminate\Support\Facades\Blade;
+use App\View\Components\IbmV18Form\Button;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,5 +31,6 @@ class AppServiceProvider extends ServiceProvider
         Blade::component('ibmv18form-input', Input::class);
         Blade::component('ibmv18form-select', Select::class);
         Blade::component('ibmv18form-textarea', Textarea::class);
+        Blade::component('ibmv18form-button', Button::class);
     }
 }
