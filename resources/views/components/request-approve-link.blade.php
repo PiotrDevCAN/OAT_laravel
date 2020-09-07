55a5f31bb58aa5e739e66c418b6ea5fa7cbad4bd
PiotrTajanowicz <PiotrTajanowicz@9.145.94.213>
2020-09-07 15:43:24 +0200

    upd

--- resources/views/components/request-approve-link.blade.php
+++ resources/views/components/request-approve-link.blade.php
@@ -1,3 +1,1 @@
-<a href=p_approveNe2.php?ref={{ $record->reference }}&amp;cat=1&amp;status=Approved&amp;via=online" target='_blank'>Approve
-	<img src='images/icon-system-status-ok.gif' width='14' height='14' alt='approve' />
-</a>
+<a href="p_approveNe2.php?ref={{ $record->reference }}&cat=1&status=Approved&via=online" target="_blank" class="ibm-confirm-link">Approve</a>
