625ca326688ca1568417fae1f971eabfe93dbfd0
PiotrTajanowicz <PiotrTajanowicz@9.145.94.213>
2020-09-07 13:49:41 +0200

    upd

--- resources/views/components/requests-table.blade.php
+++ resources/views/components/requests-table.blade.php
@@ -1,7 +1,7 @@
 <div data-widget="showhide" data-type="panel" class="ibm-show-hide ibm-alternate">
     <h2>{{ $name }} Requests</h2>
     <div class="ibm-container-body">
-        <table class="ibm-data-table ibm-altrows ibm-padding-small" data-scrollaxis="x" data-info="true" data-ordering="true" data-paging="true" data-searching="true" data-widget="datatable" id="{{ $name }}">
+        <table class="ibm-data-table ibm-altrows ibm-padding-small" data-scrollaxis="x" data-info="true" data-ordering="true" data-paging="false" data-searching="true" data-widget="datatable" id="{{ $name }}">
             <thead>
                 <tr>
                     <th>Ref</th>
