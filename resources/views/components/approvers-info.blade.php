7b2e013aa21eea5a039cc6dcddf73a1256db3db4
PiotrTajanowicz <PiotrTajanowicz@9.145.94.213>
2020-09-07 15:05:20 +0200

    upd

--- resources/views/components/approvers-info.blade.php
+++ resources/views/components/approvers-info.blade.php
@@ -1,81 +1,69 @@
 {{-- <td bgcolor="{{ $col1 }}">&nbsp;</td> --}}
 <td align='center'>
 	@empty(!$lvl1Line1)
-    	<p class="ibm-padding-bottom-0">{{ $lvl1Line1 }}</p>
+    	<p class="ibm-padding-bottom-0">
+    	<x-mailto-link :email="$lvl1Line1"/>
+    	</p>
     @endempty
     @empty(!$lvl1Line2)
     	<p class="ibm-padding-bottom-0">
-    	@if (Str::contains($lvl1Line2, '@'))
-            {{ Html::mailto($lvl1Line2) }}
-        @else
-	        {{ $lvl1Line2 }}
-        @endif
+    	<x-mailto-link :email="$lvl1Line2"/>
     	</p>
     @endempty
     @empty(!$lvl1Line3)
     	<p class="ibm-padding-bottom-0">
-    	@if (Str::contains($lvl1Line3, '@'))
-            {{ Html::mailto($lvl1Line3) }}
-        @else
-	        {{ $lvl1Line3 }}
-        @endif
+    	<x-mailto-link :email="$lvl1Line3"/>
     	</p>
     @endempty
     @empty(!$lvl1Line4)
-    	<p class="ibm-padding-bottom-0">{{ $lvl1Line4 }}</p>
+    	<p class="ibm-padding-bottom-0">
+    	<x-mailto-link :email="$lvl1Line4"/>
+    	</p>
     @endempty
 </td>
 {{-- <td bgcolor="{{ $col2 }}">&nbsp;</td> --}}
 <td align='center'>
     @empty(!$lvl2Line1)
-    	<p class="ibm-padding-bottom-0">{{ $lvl2Line1 }}</p>
+    	<p class="ibm-padding-bottom-0">
+    	<x-mailto-link :email="$lvl2Line1"/>
+    	</p>
     @endempty
     @empty(!$lvl2Line2)
     	<p class="ibm-padding-bottom-0">
-    	@if (Str::contains($lvl2Line2, '@'))
-            {{ Html::mailto($lvl2Line2) }}
-        @else
-	        {{ $lvl1Line2 }}
-        @endif
+    	<x-mailto-link :email="$lvl2Line2"/>
     	</p>
     @endempty
     @empty(!$lvl2Line3)
     	<p class="ibm-padding-bottom-0">
-    	@if (Str::contains($lvl2Line3, '@'))
-            {{ Html::mailto($lvl2Line3) }}
-        @else
-	        {{ $lvl2Line3 }}
-        @endif
+    	<x-mailto-link :email="$lvl2Line3"/>
     	</p>
     @endempty
     @empty(!$lvl2Line4)
-    	<p class="ibm-padding-bottom-0">{{ $lvl2Line4 }}</p>
+    	<p class="ibm-padding-bottom-0">
+		<x-mailto-link :email="$lvl2Line4"/>
+		</p>
     @endempty
 </td>
 {{-- <td bgcolor="{{ $col3 }}">&nbsp;</td> --}}
 <td align='center'>
 	@empty(!$lvl3Line1)
-    	<p class="ibm-padding-bottom-0">{{ $lvl3Line1 }}</p>
+    	<p class="ibm-padding-bottom-0">
+    	<x-mailto-link :email="$lvl3Line1"/>
+    	</p>
     @endempty
     @empty(!$lvl3Line2)
     	<p class="ibm-padding-bottom-0">
-    	@if (Str::contains($lvl3Line2, '@'))
-            {{ Html::mailto($lvl3Line2) }}
-        @else
-	        {{ $lvl3Line2 }}
-        @endif
+    	<x-mailto-link :email="$lvl3Line2"/>
     	</p>
     @endempty
     @empty(!$lvl3Line3)
     	<p class="ibm-padding-bottom-0">
-    	@if (Str::contains($lvl3Line3, '@'))
-            {{ Html::mailto($lvl3Line3) }}
-        @else
-	        {{ $lvl3Line3 }}
-        @endif
+    	<x-mailto-link :email="$lvl3Line3"/>
     	</p>
     @endempty
     @empty(!$lvl3Line4)
-    	<p class="ibm-padding-bottom-0">{{ $lvl3Line4 }}</p>
+    	<p class="ibm-padding-bottom-0">
+    	<x-mailto-link :email="$lvl3Line4"/>
+    	</p>
     @endempty
 </td>
