abf8893442444b742385d707e7ae0faedcec36c7
PiotrTajanowicz <PiotrTajanowicz@9.145.94.213>
2020-09-08 15:18:15 +0200

    upd

--- resources/views/components/log/table.blade.php
+++ resources/views/components/log/table.blade.php
@@ -4,49 +4,21 @@
         <table class="ibm-data-table ibm-altrows ibm-padding-small" data-scrollaxis="x" data-info="true" data-ordering="true" data-paging="true" data-searching="true" data-widget="datatable" id="{{ $name }}">
             <thead>
                 <tr>
-                    <th>Ref</th>
-                    <th>Account</th>
-                    <th>SL</th>
-                    <th>Reason</th>
-                    <th>Title</th>
-                    <th>Details</th>
-                    <th>Week Ending</th>
-                    <th>Name</th>
-                    <th>Serial</th>
-                    <th>Country</th>
-                    <th>Hours</th>
-                    <th>Status</th>
-                    <th>1st Level Approval</th>
-                    <th>2nd Level Approval</th>
-                    <th>3rd Level Approval</th>
-                    <th>Requestor</th>
-                    <th>Pre</th>
-                    <th>Post</th>
-                    <th>Claim Acc</th>
-                    <th>Created</th>
+                    <th>Edit</th>
+                    <th>Log Entry</th>
+                    <th>Last Updated</th>
+                    <th>Last Updater</th>
+                    <th>Delete</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($records as $record)
                 <tr>
-                    <td><a href="p_request.php?ref={{ $record->reference }}">{{ $record->reference }}</a></td>
-                    <td>{{ $record->account }}</td>
-                    <td>{{ $record->competency }}</td>
-                    <td>{{ $record->nature }}</td>
-                    <td>{{ $record->title }}</td>
-                    <td>{{ Str::limit($record->details, 50, ' (...)') }}</td>
-                    <td>{{ $record->weekenddate }}</td>
-                    <td><x-mailto-link :email="$record->worker"/></td>
-                    <td>{{ $record->serial }}</td>
-                    <td>{{ $record->location }}</td>
-                    <td>{{ $record->hours }}</td>
-                    <td>{{ $record->status }}</td>
-                    <x-request.approvers-info :record="$record"/>
-                    <td><x-mailto-link :email="$record->requestor"/></td>
-                    <td><a href="p_request.php?ref={{ $record->supercedes }}">{{ $record->supercedes }}</a>
-                    <td><a href="p_request.php?ref={{ $record->supercededby }}">{{ $record->supercededby }}</a></td>
-                    <td>{{ $record->claim_acc_id }}</td>
-                    <td>{{ $record->created_ts }}</td>
+                    <td>Edit Link</td>
+                    <td>{{ $record->log_entry }}</td>
+                    <td>{{ $record->last_updated }}</td>
+                    <td>{{ $record->last_updater }}</td>
+                    <td>Delete Link</td>
                 </tr>
                 @endforeach
             </tbody>
--- resources/views/logs.blade.php
+++ resources/views/logs.blade.php
@@ -4,41 +4,6 @@
 
 <x-log.filters/>
 
-<div class="ibm-fluid">
-    <div class="ibm-col-12-12">
-        <div class="ibm-card">
-            <div class="ibm-card__content">
-	        	<h3 class="ibm-bold ibm-h4 ibm-textcolor-blue-40">Logs list</h3>
-	        	
-	        	<table class="ibm-data-table" data-scrollaxis="x" data-info="true" data-ordering="true" data-paging="true" data-searching="true" data-widget="datatable" id="logTable">
-                    <thead>
-                        <tr>
-                            <th>Edit</th>
-                            <th>Log Entry</th>
-                            <th>Last Updated</th>
-                            <th>Last Updater</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($records as $record)
-                    <tr>
-                        <td>Edit Link</td>
-                        <td>{{ $record->log_entry }}</td>
-                        <td>{{ $record->last_updated }}</td>
-                        <td>{{ $record->last_updater }}</td>
-                        <td>Delete Link</td>
-                    </tr>
-                    @endforeach
-                </tbody>
-                    <tbody>
-                        
-                    </tbody>
-                </table>
-                
-	        </div>
-        </div>
-	</div>
-</div>
+<x-competency.table name="logTable" :records="$records" />
 
 @endsection
