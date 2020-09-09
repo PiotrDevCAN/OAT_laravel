fb747f7c773ff30cf72ba7a7295e6babdce26555
PiotrTajanowicz <PiotrTajanowicz@9.145.94.213>
2020-09-09 15:39:45 +0200

    upd

--- resources/views/access.blade.php
+++ resources/views/access.blade.php
@@ -7,6 +7,8 @@
         <div class="ibm-card">
             <div class="ibm-card__content">
 	        	<h3 class="ibm-bold ibm-h4 ibm-textcolor-blue-40">My Access</h3>
+	        	<p>Userid : Piotr.Tajanowicz@ibm.com</p> TO DO 
+				<p>Has access to these OAT Bluegroups : , 'OAT_Admin'</p>  TO DO 
 	        </div>
         </div>
 	</div>
