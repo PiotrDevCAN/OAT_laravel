398a79f54c16ac5e7912bc676c990c911eb30f23
PiotrTajanowicz <PiotrTajanowicz@9.145.94.213>
2020-09-10 13:46:15 +0200

    upd

--- resources/views/components/request/filters.blade.php
+++ resources/views/components/request/filters.blade.php
@@ -43,7 +43,18 @@
        	</div>
     </div>
 </div>
-
+<div class="ibm-card">
+    <div class="ibm-card__content">
+		<div class="ibm-fluid">
+            <div class="ibm-col-12-12">
+            	<p class="ibm-button-link ibm-ind-link">
+            		<a id="filterFormSubmit" class="ibm-btn-pri ibm-btn-blue-50 ibm-confirm-link" href="javascript:;" onclick="jQuery('#myForm').submit();">Apply filters</a>
+                	<a id="filterFormReset" class="ibm-btn-sec ibm-btn-blue-50 ibm-reset-link" href="javascript:;" onclick="commonDocumentList.resetFilters('#myForm')">Reset filters</a>
+               	</p>
+            </div>
+		</div>
+	</div>
+</div>
 <div class="ibm-card">
     <div class="ibm-card__content">
 		<div class="ibm-fluid">
