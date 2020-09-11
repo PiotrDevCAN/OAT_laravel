2c7ec2beaf247fe9866eca722f0ab0dfb4edcb5f
PiotrTajanowicz <PiotrTajanowicz@9.145.94.213>
2020-09-11 11:09:11 +0200

    upd

--- app/Http/Controllers/Requests.php
+++ app/Http/Controllers/Requests.php
@@ -184,7 +184,7 @@ class Requests extends Controller
             'other' => $other
         );
         
-        return view('request.index', $data);
+        return view('components.request.index', $data);
     }
     
     public function approvedList(Request $request)
@@ -200,6 +200,6 @@ class Requests extends Controller
             'approved' => $approved
         );
         
-        return view('request.index', $data);
+        return view('components.request.index', $data);
     }
 }
