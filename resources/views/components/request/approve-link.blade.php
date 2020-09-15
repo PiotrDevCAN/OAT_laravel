ed91d0d95ef2d228977683f531575f3b01a4a8c5
PiotrTajanowicz <PiotrTajanowicz@9.145.94.213>
2020-09-15 10:39:37 +0200

    upd

--- resources/views/components/request/approve-link.blade.php
+++ resources/views/components/request/approve-link.blade.php
@@ -1,1 +1,1 @@
-<a href="p_approveNe2.php?ref="{{ $record->reference }}&cat=1&status=Approved&via=online" class="ibm-bold " target="_blank" title="Approve Request" class="ibm-inlinelink ibm-confirm-link ibm-textcolor-red-50" style="color:#5aa700;">Approve</a>
+<a href="p_approveNe2.php?ref="{{ $record->reference }}&cat=1&status=Approved&via=online" target="_blank" title="Approve Request" class="ibm-bold ibm-inlinelink ibm-confirm-link ibm-textcolor-red-50" style="color:#5aa700;">Approve</a>
--- resources/views/components/request/reject-link.blade.php
+++ resources/views/components/request/reject-link.blade.php
@@ -1,1 +1,1 @@
-<a href="p_approveNe2.php?ref="{{ $record->reference }}&cat=1&status=Rejected&via=online" class="ibm-bold ibm-close-link" target="_blank" title="Reject Request" class="ibm-inlinelink ibm-reset-link ibm-textcolor-green-50" style="color:#ff5050;">Reject</a>
+<a href="p_approveNe2.php?ref="{{ $record->reference }}&cat=1&status=Rejected&via=online" target="_blank" title="Reject Request" class="ibm-bold ibm-inlinelink ibm-reset-link ibm-textcolor-green-50" style="color:#ff5050;">Reject</a>
