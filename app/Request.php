3d57787ad22bc3290e02ea73fab9f8fa2e39fd4d
PiotrTajanowicz <PiotrTajanowicz@9.145.94.213>
2020-09-15 14:08:21 +0200

    upd

--- app/Request.php
+++ app/Request.php
@@ -82,7 +82,7 @@ class Request extends Model
      */
     public function commentLogs()
     { 
-        return $this->hasOne(CommentLog::class, 'request', 'reference');
+        return $this->hasOne('App\CommentLog', 'request', 'comment');
     }
     
 }
