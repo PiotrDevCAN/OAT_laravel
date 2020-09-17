5967b8de3bf6e04ec0965f01c6162fd577b7def2
PiotrTajanowicz <PiotrTajanowicz@9.145.94.213>
2020-09-17 14:45:12 +0200

    upd

--- resources/views/components/request/approve-link.blade.php
+++ resources/views/components/request/approve-link.blade.php
@@ -1,3 +1,3 @@
 @isset($record->reference)
-<a href="{{ route('request.approve', ['ref' => $record->reference]) }}&cat=1&status=Approved&via=online" target="_blank" title="Approve Request" class="ibm-bold ibm-inlinelink ibm-confirm-link ibm-textcolor-red-50" style="color:#5aa700;">Approve</a>
+	<a href="{{ route('request.approve', ['ref' => $record->reference, 'cat' => '1', 'status' => 'Approved', 'via' => 'online']) }}" target="_blank" title="Approve Request" class="ibm-bold ibm-inlinelink ibm-confirm-link ibm-textcolor-red-50" style="color:#5aa700;">Approve</a>
 @endisset
--- resources/views/components/request/reject-link.blade.php
+++ resources/views/components/request/reject-link.blade.php
@@ -1,3 +1,3 @@
 @isset($record->reference)
-<a href="{{ route('request.reject', ['ref' => $record->reference]) }}&cat=1&status=Rejected&via=online" target="_blank" title="Reject Request" class="ibm-bold ibm-inlinelink ibm-reset-link ibm-textcolor-green-50" style="color:#ff5050;">Reject</a>
+	<a href="{{ route('request.reject', ['ref' => $record->reference, 'cat' => '1', 'status' => 'Approved', 'via' => 'online']) }}" target="_blank" title="Reject Request" class="ibm-bold ibm-inlinelink ibm-reset-link ibm-textcolor-green-50" style="color:#ff5050;">Reject</a>
 @endisset
