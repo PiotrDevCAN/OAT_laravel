e9d053edc47e7dae3847b01e15a47e9f1dc43a13
PiotrTajanowicz <PiotrTajanowicz@9.145.94.213>
2020-09-17 11:14:52 +0200

    upd

--- resources/views/components/ibmv18form/select.blade.php
+++ resources/views/components/ibmv18form/select.blade.php
@@ -9,9 +9,7 @@
     
         <select name="{{ $name }}" id="{{ $name }}" class="ibm-fullwidth">
             <option value="" selected>Select...</option>
-        	@foreach ($arrayOfSelectableValues as $key => $value)
-        	<option value="{{ trim($value->value) }}" @if($selectedValue == trim($value->value)) selected="selected" @endif>{{ trim($value->value) }}</option>
-        	@endforeach
+        	
         </select>
     </span>
 </p>
