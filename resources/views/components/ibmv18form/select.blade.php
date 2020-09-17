b850ec4226ae67b42567ef1a181775cdd21778c6
PiotrTajanowicz <PiotrTajanowicz@9.145.94.213>
2020-09-17 11:09:01 +0200

    upd

--- resources/views/components/ibmv18form/select.blade.php
+++ resources/views/components/ibmv18form/select.blade.php
@@ -1,6 +1,12 @@
 <p class="ibm-form-elem-grp">
     <label for="{{ $name }}">{{ $label }}</label>
     <span>
+    
+		    @foreach ($arrayOfSelectableValues as $key => $value)
+		    	{{ $key }}
+		    	{{ $value }}
+        	@endforeach
+    
         <select name="{{ $name }}" id="{{ $name }}" class="ibm-fullwidth">
             <option value="" selected>Select...</option>
         	@foreach ($arrayOfSelectableValues as $key => $value)
