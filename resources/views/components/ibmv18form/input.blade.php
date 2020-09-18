a9ec40b847063284c14655d37d8956d81ff9a0d8
PiotrTajanowicz <PiotrTajanowicz@9.145.94.219>
2020-09-18 14:53:20 +0200

    upd

--- resources/views/components/ibmv18form/input.blade.php
+++ resources/views/components/ibmv18form/input.blade.php
@@ -1,6 +1,6 @@
 <p class="ibm-form-elem-grp">
 	<label for="{{ $fieldName }}">{{ $label }}</label>
 	<span>
-        <input type="text" value="{{ $record->$fieldName }}" size="40" name="{{ $label }}" @isset($placeholder) placeholder="{{ $placeholder }}" @endisset @if($disabled == true) disabled @endif>
+        <input type="text" value="{{ $model->$fieldName }}" size="40" name="{{ $label }}" @isset($placeholder) placeholder="{{ $placeholder }}" @endisset @if($disabled == true) disabled @endif>
     </span>
 </p>
