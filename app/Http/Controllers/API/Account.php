9f1b8bfa89aeb34aa0baf1eac5b4b4f333fc25ff
PiotrTajanowicz <PiotrTajanowicz@9.145.2.77>
2020-09-23 13:17:31 +0200

    upd

--- app/Http/Controllers/API/Account.php
+++ app/Http/Controllers/API/Account.php
@@ -8,16 +8,6 @@ use Illuminate\Http\Request;
 class Account extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
