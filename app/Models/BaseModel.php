84de18c1ec816f8e9a52f456c55b6545cfd8839a
PiotrTajanowicz <PiotrTajanowicz@9.145.2.77>
2020-09-23 13:09:52 +0200

    upd

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -3,109 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class BaseModel extends Model
 {
-    public function save(array $options = []) {
+    
+    protected function setKeysForSaveQuery(Builder $query)
+    {
         if( ! is_array($this->getKeyName()))
         {
-            return parent::save($options);
+            return parent::setKeysForSaveQuery($query);
         }
         
-        // Fire Event for others to hook
-        if($this->fireModelEvent('saving') === false) return false;
+        //
+        // START FIX
+        //
         
-        // Prepare query for inserting or updating
-        $query = $this->newQueryWithoutScopes();
+        // Convert primary key into an array if it's a single value
+        $primary = (count($this->getKeyName()) > 1) ? $this->getKeyName() : [$this->getKeyName()];
         
-        // Perform Update
-        if ($this->exists)
-        {
-            if (count($this->getDirty()) > 0)
-            {
-                // Fire Event for others to hook
-                if ($this->fireModelEvent('updating') === false)
-                {
-                    return false;
-                }
-                
-                // Touch the timestamps
-                if ($this->timestamps)
-                {
-                    $this->updateTimestamps();
-                }
-                
-                //
-                // START FIX
-                //
-                
-                
-                // Convert primary key into an array if it's a single value
-                $primary = (count($this->getKeyName()) > 1) ? $this->getKeyName() : [$this->getKeyName()];
-                
-                // Fetch the primary key(s) values before any changes
-                $unique = array_intersect_key($this->original, array_flip($primary));
-                
-                // Fetch the primary key(s) values after any changes
-                $unique = !empty($unique) ? $unique : array_intersect_key($this->getAttributes(), array_flip($primary));
-                
-                // Fetch the element of the array if the array contains only a single element
-                //$unique = (count($unique) <> 1) ? $unique : reset($unique);
-                
-                // Apply SQL logic
-                $query->where($unique);
-                
-                //
-                // END FIX
-                //
-                
-                // Update the records
-                $query->update($this->getDirty());
-                
-                // Fire an event for hooking into
-                $this->fireModelEvent('updated', false);
-            }
-        }
-        // Insert
-        else
-        {
-            // Fire an event for hooking into
-            if ($this->fireModelEvent('creating') === false) return false;
-            
-            // Touch the timestamps
-            if($this->timestamps)
-            {
-                $this->updateTimestamps();
-            }
-            
-            // Retrieve the attributes
-            $attributes = $this->attributes;
-            
-            if ($this->incrementing && !is_array($this->getKeyName()))
-            {
-                $this->insertAndSetId($query, $attributes);
-            }
-            else
-            {
-                $query->insert($attributes);
-            }
-            
-            // Set exists to true in case someone tries to update it during an event
-            $this->exists = true;
-            
-            // Fire an event for hooking into
-            $this->fireModelEvent('created', false);
-        }
+        // Fetch the primary key(s) values before any changes
+        $unique = array_intersect_key($this->original, array_flip($primary));
         
-        // Fires an event
-        $this->fireModelEvent('saved', false);
+        // Fetch the primary key(s) values after any changes
+        $unique = !empty($unique) ? $unique : array_intersect_key($this->getAttributes(), array_flip($primary));
         
-        // Sync
-        $this->original = $this->attributes;
+        // Apply SQL logic
+        $query->where($unique);
         
-        // Touches all relations
-        if (array_get($options, 'touch', true)) $this->touchOwners();
+        //
+        // END FIX
+        //
         
-        return true;
+        return $query;
     }
 }
