48b039f037fa5fde36a5487d9eec49fae8228571
PiotrTajanowicz <PiotrTajanowicz@9.145.2.77>
2020-09-24 12:49:31 +0200

    upd

--- resources/views/components/request/reject-link.blade.php
+++ resources/views/components/request/reject-link.blade.php
@@ -1,3 +1,3 @@
 @isset($record->reference)
-	{{ link_to_route('request.reject', $title = 'Reject Request', $parameters = ['overtimeRequest' => $record->reference, 'lvl' => '1', 'status' => 'Approved', 'via' => 'online'], $attributes = ['class' => 'ibm-bold ibm-inlinelink ibm-confirm-link ibm-textcolor-red-50', 'style' => 'color:#ff5050;']) }}
+	{{ link_to_route('request.reject', $title = 'Reject Request', $parameters = ['overtimeRequest' => $record->reference, 'lvl' => '1', 'status' => 'Approved', 'via' => 'online'], $attributes = ['class' => 'ibm-bold ibm-inlinelink ibm-reset-link ibm-textcolor-red-50', 'style' => 'color:#ff5050;']) }}
 @endisset
