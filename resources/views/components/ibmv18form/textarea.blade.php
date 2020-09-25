eca6531a05b99f5fa4a2798571b642542c729ac3
PiotrTajanowicz <PiotrTajanowicz@9.145.2.77>
2020-09-25 12:54:06 +0200

    upd

--- resources/views/components/ibmv18form/textarea.blade.php
+++ resources/views/components/ibmv18form/textarea.blade.php
@@ -1,6 +1,6 @@
 <p>
 	{{ Form::label($fieldName, $label, $labelOptions) }}
     <span>
-        {{ Form::textarea($fieldName, $value, $options}}
+        {{ Form::textarea($fieldName, $value, $options) }}
     </span>
 </p>
