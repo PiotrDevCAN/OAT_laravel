1617f8df5750f5df452135a394a975dd438c20da
PiotrTajanowicz <PiotrTajanowicz@9.145.2.77>
2020-09-28 12:57:44 +0200

    upd

--- app/Mail/OvertimeRequestCreated.php
+++ app/Mail/OvertimeRequestCreated.php
@@ -7,16 +7,6 @@ use App\Models\OvertimeRequest;
 class OvertimeRequestCreated extends OvertimeRequestBase
 {
     /**
-     * Build the message.
-     *
-     * @return $this
-     */
-    public function build()
-    {
-        return $this->markdown('emails.request.created');
-    }
-    
-    /**
      * Create a new message instance.
      *
      * @return void
@@ -24,5 +14,15 @@ class OvertimeRequestCreated extends OvertimeRequestBase
     public function __construct(OvertimeRequest $overtimeRequest)
     {
         parent::__construct($overtimeRequest);
+    }
+    
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build(OvertimeRequest $overtimeRequest)
+    {
+        return $this->markdown('emails.request.created');
     }
 }
