27f12f237bbfbcbb0a22a6a28d1eaf67c3f9e22c
PiotrTajanowicz <PiotrTajanowicz@9.145.81.184>
2020-10-01 10:57:40 +0200

    upd

---
+++ app/Listeners/LogKeyForgotten.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Cache\Events\KeyForgotten;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class LogKeyForgotten
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  KeyForgotten  $event
+     * @return void
+     */
+    public function handle(KeyForgotten $event)
+    {
+        //
+    }
+}
---
+++ app/Listeners/LogKeyWritten.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Cache\Events\KeyWritten;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class LogKeyWritten
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  KeyWritten  $event
+     * @return void
+     */
+    public function handle(KeyWritten $event)
+    {
+        //
+    }
+}
