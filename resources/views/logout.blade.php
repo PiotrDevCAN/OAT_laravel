9e0b5dc740b5103536e8717b40b5b0176d5cbafc
PiotrTajanowicz <PiotrTajanowicz@9.145.81.184>
2020-10-02 10:18:42 +0200

    upd

--- resources/views/logout.blade.php
+++ resources/views/logout.blade.php
@@ -1,28 +1,3 @@
-<?php
-// use sessions to store authentication and authorization state
-@session_start();
-ob_start();
-$ajaxCall = isset($ajaxCall) ? $ajaxCall : false;
-$https = (isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == "on");
-
-// Only keep login data available via secure sessions
-if (isset($_SESSION['ltcuser']) && $https === FALSE) {
-    //unset($_SESSION['ltcuser']);
-}
-// restore cached logins if not expired (expiry was set at last logon)
-// if (isset($_SESSION['ltcuser']['expire'])) {
-//     if ($_SESSION['ltcuser']['expire'] > time()) {
-		if (isset($_SESSION['ltcuser'])) {
-        	$GLOBALS['ltcuser'] = $_SESSION['ltcuser'];
-		}
-//     } else {
-//         unset($GLOBALS['ltcuser']);
-//         unset($_SESSION['ltcuser']);
-//     }
-// }
-
-?>
-
 <style type="text/css">
 html, body {
 	background: url(../public/img/logon/think_travel_hero.png) no-repeat
@@ -35,18 +10,11 @@ html, body {
 </style>
 </head>
 <body>
-
-<?php
-session_destroy();
-unset($GLOBALS['ltcuser']);
-unset($_SESSION['ltcuser']);
-?>
-
 <div class="ibm-fluid ibm-padding-top-90 ibm-padding-bottom-90" data-always="true" data-items=".ibm-card">
 	<div class="ibm-col-12-6">
 		<div class="ibm-card">
 			<div class="ibm-card__content">
-				<p class="ibm-h3 ibm-light ibm-textcolor-blue-60 ibm-padding-bottom-0"><?php echo $site['site_title'];?></p>
+				<p class="ibm-h3 ibm-light ibm-textcolor-blue-60 ibm-padding-bottom-0">{{ config('app.name') }}</p>
 			</div>
 		</div>
 	</div>
