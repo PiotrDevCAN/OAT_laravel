eb0e5bc22a65527a38618b1c5c57cdde5eb5c1eb
PiotrTajanowicz <PiotrTajanowicz@9.145.25.196>
2020-10-05 16:14:21 +0200

    upd

--- app/Extensions/IBMUserProvider.php
+++ app/Extensions/IBMUserProvider.php
@@ -147,7 +147,7 @@ class IBMUserProvider implements UserProvider
                 $model->email = 'Piotr.Tajanowicz@ibm.com';
                 $model->password = 'ABC';
                 
-                return $model;
+                return collect($model);
     }
 
     /**
