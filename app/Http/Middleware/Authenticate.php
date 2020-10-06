66e5e1f3bdf6c6ca5860978063eeeb35496b2b7a
PiotrTajanowicz <PiotrTajanowicz@9.145.25.196>
2020-10-06 15:57:44 +0200

    upd

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -6,6 +6,24 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
+    
+    protected function authenticate($request, array $guards)
+    {
+        if (empty($guards)) {
+            $guards = [null];
+        }
+        
+        dump($guards);
+        
+        foreach ($guards as $guard) {
+            if ($this->auth->guard($guard)->check()) {
+                return $this->auth->shouldUse($guard);
+            }
+        }
+        
+//         $this->unauthenticated($request, $guards);
+    }
+    
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
