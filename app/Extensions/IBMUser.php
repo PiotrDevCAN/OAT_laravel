2a2a9ce9998f1d473eeeb3100b90d8afbb376204
PiotrTajanowicz <PiotrTajanowicz@9.145.25.196>
2020-10-06 16:24:10 +0200

    upd

--- app/Extensions/IBMUser.php
+++ app/Extensions/IBMUser.php
@@ -12,39 +12,4 @@ class IBMUser extends GenericUser
         
         return 'cnum';
     }
-    
-//     public function getAuthIdentifier()
-//     {
-//         dump('createModel');
-        
-//         return 'IBM';
-//     }
-    
-//     public function getAuthPassword()
-//     {
-//         dump('createModel');
-        
-//         return 'ABC';
-//     }
-    
-//     public function getRememberToken()
-//     {
-//         dump('createModel');
-        
-//         return 'ABC';
-//     }
-    
-//     public function setRememberToken($value)
-//     {
-//         dump('createModel');
-        
-//         return 'ABC';
-//     }
-    
-//     public function getRememberTokenName()
-//     {
-//         dump('createModel');
-        
-//         return 'ABC';
-//     }
 }
