2fa61bb8bc77ff65386cee25c61e9008e9584528
PiotrTajanowicz <PiotrTajanowicz@9.145.71.146>
2020-10-11 15:34:06 +0200

    upd

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
@@ -15,8 +14,8 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+//         if (! $request->expectsJson()) {
+//             return route('login');
+//         }
     }
 }
