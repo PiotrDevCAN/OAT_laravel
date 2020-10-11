6beb57b48abf509e0f2635029f4e83f3953f8943
PiotrTajanowicz <PiotrTajanowicz@9.145.71.146>
2020-10-11 15:19:00 +0200

    upd

--- app/Models/IBMUser.php
+++ app/Models/IBMUser.php
@@ -13,6 +13,22 @@ class IBMUser implements UserContract
      */
     protected $attributes;
     
+    /**
+     * Create a new generic User object.
+     *
+     * @param  array  $attributes
+     * @return void
+     */
+    public function __construct(array $attributes)
+    {
+        $this->attributes = $attributes;
+    }
+
+    public function getAuthIdentifierName()
+    {
+        return 'cnum';
+    }
+
     public function getAuthIdentifier()
     {
         return $this->attributes[$this->getAuthIdentifierName()];
@@ -38,9 +54,49 @@ class IBMUser implements UserContract
         $this->attributes[$this->getRememberTokenName()] = $value;
     }
 
-    public function getAuthIdentifierName()
+    /**
+     * Dynamically access the user's attributes.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function __get($key)
     {
-        return 'cnum';
+        return $this->attributes[$key];
+    }
+    
+    /**
+     * Dynamically set an attribute on the user.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return void
+     */
+    public function __set($key, $value)
+    {
+        $this->attributes[$key] = $value;
+    }
+    
+    /**
+     * Dynamically check if a value is set on the user.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function __isset($key)
+    {
+        return isset($this->attributes[$key]);
+    }
+    
+    /**
+     * Dynamically unset a value on the user.
+     *
+     * @param  string  $key
+     * @return void
+     */
+    public function __unset($key)
+    {
+        unset($this->attributes[$key]);
     }
 }
 
