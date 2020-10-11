842891858343c979fb3d7a2d227f44e2beefdfe3
PiotrTajanowicz <PiotrTajanowicz@9.145.71.146>
2020-10-11 15:20:42 +0200

    upd

--- app/Models/IBMUser.php
+++ app/Models/IBMUser.php
@@ -2,101 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\Authenticatable as UserContract;
+use Illuminate\Auth\GenericUser;
 
-class IBMUser implements UserContract
+class IBMUser extends GenericUser
 {
-    /**
-     * All of the user's attributes.
-     *
-     * @var array
-     */
-    protected $attributes;
-    
-    /**
-     * Create a new generic User object.
-     *
-     * @param  array  $attributes
-     * @return void
-     */
-    public function __construct(array $attributes)
-    {
-        $this->attributes = $attributes;
-    }
-
     public function getAuthIdentifierName()
     {
         return 'cnum';
     }
-
-    public function getAuthIdentifier()
-    {
-        return $this->attributes[$this->getAuthIdentifierName()];
-    }
-
-    public function getRememberToken()
-    {
-        return $this->attributes[$this->getRememberTokenName()];
-    }
-
-    public function getAuthPassword()
-    {
-        return $this->attributes['password'];
-    }
-
-    public function getRememberTokenName()
-    {
-        return 'remember_token';
-    }
-
-    public function setRememberToken($value)
-    {
-        $this->attributes[$this->getRememberTokenName()] = $value;
-    }
-
-    /**
-     * Dynamically access the user's attributes.
-     *
-     * @param  string  $key
-     * @return mixed
-     */
-    public function __get($key)
-    {
-        return $this->attributes[$key];
-    }
-    
-    /**
-     * Dynamically set an attribute on the user.
-     *
-     * @param  string  $key
-     * @param  mixed  $value
-     * @return void
-     */
-    public function __set($key, $value)
-    {
-        $this->attributes[$key] = $value;
-    }
-    
-    /**
-     * Dynamically check if a value is set on the user.
-     *
-     * @param  string  $key
-     * @return bool
-     */
-    public function __isset($key)
-    {
-        return isset($this->attributes[$key]);
-    }
-    
-    /**
-     * Dynamically unset a value on the user.
-     *
-     * @param  string  $key
-     * @return void
-     */
-    public function __unset($key)
-    {
-        unset($this->attributes[$key]);
-    }
 }
-
