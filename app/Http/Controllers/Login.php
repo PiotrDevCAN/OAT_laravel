42b780baa93d8981ab925a35c9edfd33c8bbde52
PiotrTajanowicz <PiotrTajanowicz@9.145.71.146>
2020-10-11 14:39:53 +0200

    upd

--- app/Http/Controllers/Login.php
+++ app/Http/Controllers/Login.php
@@ -46,7 +46,7 @@ class Login extends Controller
             
                 // validation successful!
                 // redirect them to the secure section or whatever
-                return Redirect::route('home');
+//                 return Redirect::route('home');
             
             } else {
                 
