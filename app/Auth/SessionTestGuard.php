754ee891cd12642a162e38fada4834228c66c814
PiotrTajanowicz <PiotrTajanowicz@9.145.71.146>
2020-10-12 13:06:47 +0200

    upd

--- app/Auth/SessionTestGuard.php
+++ app/Auth/SessionTestGuard.php
@@ -96,7 +96,7 @@ class SessionTestGuard extends SessionGuard
         dump('SessionTestGuard check');
         dump('Saved user');
         dump($this->user());
-        dump(is_null($this->user());
+        dump(is_null($this->user()));
         
         return ! is_null($this->user());
     }
