f460066eb4006c853e3fc01e5277c24492aabd16
PiotrTajanowicz <PiotrTajanowicz@9.145.71.146>
2020-10-12 11:40:07 +0200

    upd

--- app/Auth/IBMUserGuard.php
+++ app/Auth/IBMUserGuard.php
@@ -5,15 +5,12 @@ namespace App\Auth;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Http\Request;
-use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\GuardHelpers;
 
 class IBMUserGuard implements StatefulGuard
 {
-    /**
-     * @var null|Authenticatable|User
-     */
-    protected $user;
-    
+    use GuardHelpers;
+
     /**
      * @var Request
      */
@@ -30,11 +27,8 @@ class IBMUserGuard implements StatefulGuard
     }
 
     public function viaRemember()
-    {}
-
-    public function check(): bool
     {
-        return (bool)$this->user();
+        return $this->viaRemember;
     }
 
     public function login(Authenticatable $user, $remember = false)
@@ -59,45 +53,46 @@ class IBMUserGuard implements StatefulGuard
     public function once(array $credentials = [])
     {}
 
-    public function guest(): bool
+    public function user()
     {
-        return !$this->check();
-    }
-
-    public function id(): ?int
-    {
-        $user = $this->user();
-        return $user->id ?? null;
-    }
-
-    public function setUser(?Authenticatable $user): self
-    {
-        $this->user = $user;
-        return $this;
-    }
-
-    public function user(): ? User
-    {
-        return $this->user ?: $this->signInWithPin();
+//         if ($this->loggedOut) {
+//             return;
+//         }
+        
+        // If we've already retrieved the user for the current request we can just
+        // return it back immediately. We do not want to fetch the user data on
+        // every call to this method because that would be tremendously slow.
+//         if (! is_null($this->user)) {
+//             return $this->user;
+//         }
+        
+//         $id = $this->session->get($this->getName());
+        
+//         // First we will try to load the user using the identifier in the session if
+//         // one exists. Otherwise we will check for a "remember me" cookie in this
+//         // request, and if one exists, attempt to retrieve the user using that.
+//         if (! is_null($id) && $this->user = $this->provider->retrieveById($id)) {
+//             $this->fireAuthenticatedEvent($this->user);
+//         }
+        
+//         // If the user is null, but we decrypt a "recaller" cookie we can attempt to
+//         // pull the user data on that cookie which serves as a remember cookie on
+//         // the application. Once we have a user we can return it to the caller.
+//         if (is_null($this->user) && ! is_null($recaller = $this->recaller())) {
+//             $this->user = $this->userFromRecaller($recaller);
+            
+//             if ($this->user) {
+//                 $this->updateSession($this->user->getAuthIdentifier());
+                
+//                 $this->fireLoginEvent($this->user, true);
+//             }
+//         }
+        
+        return $this->user;
     }
 
     public function validate(array $credentials = []): bool
     {
         throw new \BadMethodCallException('Unexpected method call');
-    }
-    
-    public function authenticate(): User
-    {
-        $user = $this->user();
-        if ($user instanceof User) {
-            return $user;
-        }
-        throw new AuthenticationException();
-    }
-
-    protected function signInWithPin(): ? User
-    {
-        // Implement your logic here
-        // Return User on success, or return null on failure
     }
 }
