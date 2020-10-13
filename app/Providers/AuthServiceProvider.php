4b60827deaad5944acf6aead59163b085f0d22fb
PiotrTajanowicz <PiotrTajanowicz@9.145.71.146>
2020-10-13 13:07:02 +0200

    upd

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -34,6 +34,48 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+    
+        Auth::extend('bluepage-guard', function ($app, $name, array $config) {
+            return $app->make(BluepageUserGuard::class, [
+                'name' => $name,
+                'provider' => $app['auth']->createUserProvider(
+                    $config['provider'] ?? null
+                    ),
+                'session' => $app['session.store']
+            ]);
+        });
+            
+        Auth::provider('bluepage-provider', function ($app, array $config) {
+            // Return an instance of Illuminate\Contracts\Auth\UserProvider...
+            
+            return $app->make(BluepageUserProvider::class, [
+                'hasher' => $app['hash'],
+            ]);
+        });
+        
+        Auth::extend('bluegroup-guest-guard', function ($app, $name, array $config) {
+            return $app->make(BluegroupGuestUserGuard::class);
+        });
+        
+        Auth::provider('bluegroup-guest-provider', function ($app, array $config) {
+            // Return an instance of Illuminate\Contracts\Auth\UserProvider...
+            
+            return $app->make(BluegroupGuestUserProvider::class, [
+                'hasher' => $app['hash'],
+            ]);
+        });
+        
+        Auth::extend('bluegroup-admin-guard', function ($app, $name, array $config) {
+            return $app->make(BluegroupAdminUserGuard::class);
+        });
+        
+        Auth::provider('bluegroup-admin-provider', function ($app, array $config) {
+            // Return an instance of Illuminate\Contracts\Auth\UserProvider...
+            
+            return $app->make(BluegroupAdminUserProvider::class, [
+                'hasher' => $app['hash'],
+            ]);
+        });
     }
 
     /**
@@ -46,6 +88,7 @@ class AuthServiceProvider extends ServiceProvider
         // Since the Auth Manager doesn't have to be called in every Request, we will just
         // set a callback before the application resolves it and passes it where it was
         // called.
+        /*
         $this->app->resolving('auth', function ($auth) {
             
             Auth::extend('bluepage-guard', function ($app, $name, array $config) {
@@ -90,5 +133,6 @@ class AuthServiceProvider extends ServiceProvider
                 ]);
             });
         });
+        */
     }
 }
