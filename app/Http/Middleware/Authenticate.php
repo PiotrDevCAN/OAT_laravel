a036fd3b5436a9b4d7484ffab4b2e9683a106e2d
PiotrTajanowicz <PiotrTajanowicz@9.145.71.146>
2020-10-13 14:54:19 +0200

    upd

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Support\Facades\Auth;    
+use Illuminate\Auth\AuthenticationException;
 
 class Authenticate extends Middleware
 {
@@ -56,6 +57,9 @@ class Authenticate extends Middleware
         dump($user);
         
 //         $this->unauthenticated($request, $guards);
+        throw new AuthenticationException(
+            'Unauthenticated.', $guards
+        );
     }
     
     /**
