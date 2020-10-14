c9368cb1cf7d0d6669580472d71bb1344ac575dc
PiotrTajanowicz <PiotrTajanowicz@9.145.71.146>
2020-10-14 15:41:25 +0200

    upd

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,8 +32,8 @@ class Kernel extends HttpKernel
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            // \Illuminate\Session\Middleware\StartSession::class,
-            \App\Http\Middleware\StartMySession::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            // \App\Http\Middleware\StartMySession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
