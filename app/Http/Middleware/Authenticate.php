e374375c257f8a2fc863b1335d63b4a965b6d24e
PiotrTajanowicz <PiotrTajanowicz@9.145.71.146>
2020-10-14 12:59:52 +0200

    upd

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -28,7 +28,31 @@ class Authenticate extends Middleware
         
         return $next($request);
     }
-    
+
+    /**
+     * Determine if the user is logged in to any of the given guards.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  array  $guards
+     * @return void
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    protected function authenticate($request, array $guards)
+    {
+        if (empty($guards)) {
+            $guards = [null];
+        }
+        
+        foreach ($guards as $guard) {
+            if ($this->auth->guard($guard)->check()) {
+                return $this->auth->shouldUse($guard);
+            }
+        }
+        
+//         $this->unauthenticated($request, $guards);
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
