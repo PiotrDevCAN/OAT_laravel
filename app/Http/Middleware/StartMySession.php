841507ae9d08afae4ba2fdf88c31f415a9ef36c6
PiotrTajanowicz <PiotrTajanowicz@9.145.71.146>
2020-10-14 13:56:09 +0200

    upd

--- app/Http/Middleware/StartMySession.php
+++ app/Http/Middleware/StartMySession.php
@@ -43,4 +43,40 @@ class StartMySession extends StartSession
                 return $this->handleStatefulRequest($request, $session, $next);
             }
     }
+    
+    /**
+     * Handle the given request within session state.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Contracts\Session\Session  $session
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    protected function handleStatefulRequest(Request $request, $session, Closure $next)
+    {
+        // If a session driver has been configured, we will need to start the session here
+        // so that the data is ready for an application. Note that the Laravel sessions
+        // do not make use of PHP "native" sessions in any way since they are crappy.
+        $request->setLaravelSession(
+            $this->startSession($request, $session)
+            );
+        
+        $this->collectGarbage($session);
+        
+        $response = $next($request);
+        
+        $this->storeCurrentUrl($request, $session);
+        
+        $this->addCookieToResponse($response, $session);
+        
+        // Again, if the session has been configured we will need to close out the session
+        // so that the attributes may be persisted to some storage medium. We will also
+        // add the session identifier cookie to the application response headers now.
+        $this->saveSession($request);
+        
+        dump('UPDATED SESSION');
+        dump($session);
+        
+        return $response;
+    }
 }
