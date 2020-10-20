3fa54e6720bf42803dbbefc5ad073551cdf8c783
PiotrTajanowicz <PiotrTajanowicz@9.145.183.118>
2020-10-20 10:09:51 +0200

    upd

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\OvertimeRequests;
+use App\Http\Controllers\API\Accounts;
+use App\Http\Controllers\API\Delegates;
+use App\Http\Controllers\API\Competencies;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,66 +26,66 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 // Overtime Requests
 Route::prefix('request')->name('request.')->group(function () {
-    Route::get('store', 'API\OvertimeRequests@store')
+    Route::get('store', [OvertimeRequests::class, 'store'])
         ->name('store');
 
-    Route::get('show/{ref}', 'OvertimeRequests@show')
+    Route::get('show/{ref}', [OvertimeRequests::class, 'show'])
         ->name('show');
 
-    Route::get('update/{ref}', 'OvertimeRequests@update')
+    Route::get('update/{ref}', [OvertimeRequests::class, 'update'])
         ->name('update');
 
-    Route::get('destroy/{ref}', 'OvertimeRequests@destroy')
+    Route::get('destroy/{ref}', [OvertimeRequests::class, 'destroy'])
         ->name('destroy');
 
-    Route::get('approve/{overtimeRequest}/cat/{lvl?}/status/{status?}/via/{via?}', 'OvertimeRequests@approve')
+    Route::get('approve/{overtimeRequest}/cat/{lvl?}/status/{status?}/via/{via?}', [OvertimeRequests::class, 'approve'])
         ->name('approve');
 
-    Route::get('reject/{overtimeRequest}/cat/{lvl?}/status/{status?}/via/{via?}', 'OvertimeRequests@reject')
+    Route::get('reject/{overtimeRequest}/cat/{lvl?}/status/{status?}/via/{via?}', [OvertimeRequests::class, 'reject'])
         ->name('reject');
 });
 
 // Accounts
 Route::prefix('account')->name('account.')->group(function () {
-    Route::get('store', 'Accounts@store')
+    Route::get('store', [Accounts::class, 'store'])
         ->name('store');
     
-    Route::get('show/{account}/{location}', 'Accounts@show')
+    Route::get('show/{account}/{location}', [Accounts::class, 'show'])
         ->name('show');
         
-    Route::get('update/{account}/{location}', 'Accounts@update')
+    Route::get('update/{account}/{location}', [Accounts::class, 'update'])
         ->name('update');
 
-    Route::get('destroy/{account}/{location}', 'Accounts@destroy')
+    Route::get('destroy/{account}/{location}', [Accounts::class, 'destroy'])
         ->name('destroy');
 });
 
 // Delegates
 Route::prefix('delegate')->name('delegate.')->group(function () {
-    Route::get('store', 'Delegates@store')
+    Route::get('store', [Delegates::class, 'store'])
         ->name('store');
         
-    Route::get('show/{user_intranet}/{delegate_intranet}', 'Delegates@show')
+    Route::get('show/{user_intranet}/{delegate_intranet}', [Delegates::class, 'show'])
         ->name('show');
         
-    Route::get('update/{user_intranet}/{delegate_intranet}', 'Delegates@update')
+    Route::get('update/{user_intranet}/{delegate_intranet}', [Delegates::class, 'update'])
         ->name('update');
 
-    Route::get('destroy/{user_intranet}/{delegate_intranet}', 'Delegates@destroy')
+    Route::get('destroy/{user_intranet}/{delegate_intranet}', [Delegates::class, 'destroy'])
         ->name('destroy');
 });
 
 // Competencies
 Route::prefix('competency')->name('competency.')->group(function () {
-    Route::get('store', 'Competencies@store')
+    Route::get('store', [Competencies::class, 'store'])
         ->name('store');
-        
-    Route::get('show/{competency}/{approver}', 'Competencies@show')
+
+    Route::get('show/{competency}/{approver}', [Competencies::class, 'show'])
         ->name('show');
-        
-    Route::get('update/{competency}/{approver}', 'Competencies@update')
+
+    Route::get('update/{competency}/{approver}', [Competencies::class, 'update'])
         ->name('update');
 
-    Route::get('destroy/{competency}/{approver}', 'Competencies@destroy')
+    Route::get('destroy/{competency}/{approver}', [Competencies::class, 'destroy'])
         ->name('destroy');
 });
