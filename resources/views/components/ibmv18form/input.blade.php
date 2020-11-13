14f0fb708616b545e15de03ccf6a2bf16723c1fc
PiotrTajanowicz <PiotrTajanowicz@9.145.156.112>
2020-11-13 19:44:35 +0100

    upd

--- resources/views/components/ibmv18form/input.blade.php
+++ resources/views/components/ibmv18form/input.blade.php
@@ -1,6 +1,9 @@
 <p class="ibm-form-elem-grp">
     {{ Form::label($fieldName, $label) }}
     <span>
+    
+    {{ $selectedValue }}
+    
     	{{ Form::text($fieldName, $selectedValue, $options) }}
     </span>
 </p>
