beec62946e91118606fc1bccae90d6405494e629
PiotrTajanowicz <PiotrTajanowicz@9.145.58.168>
2020-11-19 14:15:47 +0100

    upd

--- resources/views/components/request/table.blade.php
+++ resources/views/components/request/table.blade.php
@@ -1,43 +1,43 @@
 <div data-widget="showhide" data-type="panel" class="ibm-show-hide ibm-alternate">
     <h2 @if($expand == true)data-open="true"@endif>{{ $name }} Requests</h2>
     <div class="ibm-container-body">
-        <table class="ibm-data-table ibm-altrows ibm-small" data-scrollaxis="x" data-info="true" data-ordering="true" data-paging="true" data-searching="true" data-widget="datatable" id="{{ $name }}">
+        <table class="ibm-data-table ibm-altrows ibm-padding-small ibm-small" data-scrollaxis="x" data-info="true" data-ordering="true" data-paging="true" data-searching="true" data-widget="datatable" id="{{ $name }}">
             <thead>
                 <tr>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">Ref</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">Account</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">SL</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">Reason</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">Title</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">Details</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">Week Ending</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">Name</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">Serial</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">Country</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">Hours</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">Status</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">1st Level Approval</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">2nd Level Approval</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">3rd Level Approval</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">Requestor</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">Approval Flow</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">Squad Leader</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">Tribe Leader</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">Pre</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">Post</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">Claim Acc</th>
-                    <th class="ibm-padding-top-0 ibm-padding-bottom-0">Created</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">Ref</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">Account</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">SL</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">Reason</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">Title</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">Details</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">Week Ending</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">Name</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">Serial</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">Country</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">Hours</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">Status</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">1st Level Approval</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">2nd Level Approval</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">3rd Level Approval</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">Requestor</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">Approval Flow</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">Squad Leader</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">Tribe Leader</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">Pre</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">Post</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">Claim Acc</th>
+                    <th class="ibm-padding-top-0 ibm-padding-bottom-0 ibm-padding-right-0">Created</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($records as $key => $record)
                 <tr>
-                    <td>
+                    <td class="ibm-nospacing">
                     @isset($record->reference)
                     	{{ link_to_route('request.show', $title = $record->reference, ['overtimeRequest' => $record->reference]) }}
                     @endisset
                     </td>
-                    <td class="ibm-bold">
+                    <td class="ibm-nospacing ibm-bold">
                     	{{ $record->account }}
                     	@isset($record->comment)
                     		<p class="ibm-ind-link ibm-icononly">
@@ -50,10 +50,10 @@
                             </div>
                         @endisset
                 	</td>
-                    <td>{{ $record->competency }}</td>
-                    <td>{{ $record->nature }}</td>
-                    <td>{{ $record->title }}</td>
-                    <td>
+                    <td class="ibm-nospacing">{{ $record->competency }}</td>
+                    <td class="ibm-nospacing">{{ $record->nature }}</td>
+                    <td class="ibm-nospacing">{{ $record->title }}</td>
+                    <td class="ibm-nospacing">
                     {{ Str::limit($record->details, 50, '') }}
                     @if(Str::length($record->details) >= 50)
 	                    <a href="#" class="ibm-bold" data-widget="tooltip" data-contentid="preview-{{ $name }}-{{ $key }}" style="text-decoration: none;">(...)</a>
@@ -62,30 +62,30 @@
                         </div>
                     @endif
                     </td>
-                    <td>{{ $record->weekenddate }}</td>
+                    <td class="ibm-nospacing">{{ $record->weekenddate }}</td>
                     <td ><x-mailto-link :email="$record->worker"/></td>
-                    <td>{{ $record->serial }}</td>
-                    <td>{{ $record->location }}</td>
-                    <td>{{ $record->hours }}</td>
+                    <td class="ibm-nospacing">{{ $record->serial }}</td>
+                    <td class="ibm-nospacing">{{ $record->location }}</td>
+                    <td class="ibm-nospacing">{{ $record->hours }}</td>
                     <td class="ibm-bold">{{ $record->status }}</td>
                     <x-request.approvers-info :record="$record"/>
-                    <td><x-mailto-link :email="$record->requestor"/></td>
-                    <td>{{ $record->approval_mode }}
+                    <td class="ibm-nospacing"><x-mailto-link :email="$record->requestor"/></td>
+                    <td class="ibm-nospacing">{{ $record->approval_mode }}
                     Change flow to...
                     </td>
-                    <td><x-mailto-link :email="$record->approver_squad_leader"/></td>
-                    <td><x-mailto-link :email="$record->approver_tribe_leader"/></td>
-                    <td>
+                    <td class="ibm-nospacing"><x-mailto-link :email="$record->approver_squad_leader"/></td>
+                    <td class="ibm-nospacing"><x-mailto-link :email="$record->approver_tribe_leader"/></td>
+                    <td class="ibm-nospacing">
                     @isset($record->supercedes)
                     	{{ link_to_route('request.show', $title = $record->supercedes, ['overtimeRequest' => $record->supercedes]) }}
                     @endisset
-                    <td>
+                    <td class="ibm-nospacing">
                     @isset($record->supercededby)
                    		{{ link_to_route('request.show', $title = $record->supercededby, ['overtimeRequest' => $record->supercededby]) }}
                     @endisset
                     </td>
-                    <td>{{ $record->claim_acc_id }}</td>
-                    <td>{{ $record->created_ts }}</td>
+                    <td class="ibm-nospacing">{{ $record->claim_acc_id }}</td>
+                    <td class="ibm-nospacing">{{ $record->created_ts }}</td>
                 </tr>
                 @endforeach
             </tbody>
