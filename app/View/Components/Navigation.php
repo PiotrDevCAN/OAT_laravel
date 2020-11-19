d78e69e29237920d11f10337ff5ccce0be769681
PiotrTajanowicz <PiotrTajanowicz@9.145.58.168>
2020-11-19 17:11:41 +0100

    upd

--- app/View/Components/Navigation.php
+++ app/View/Components/Navigation.php
@@ -81,33 +81,31 @@ class Navigation extends Component
             ),
         );
         
-        /*
         foreach ($this->menuList as $key => $value) {
             if (is_array($value['route'])) {
                 foreach ($value['route'] as $subKey => $subValue) {
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-//                     foreach ($subValue as $subSubKey => $subSubValue) {                        
-//                         if (isset($subSubValue['route'])) {
-//                             if ($subSubValue['route'] == Route::currentRouteName()) {
-//                                 $this->menuList[$key]['route'][$subSubKey]['selected'] = true;
-//                             }
-//                         } else {
-//                             foreach ($subSubValue as $subSubSubKey => $subSubSubValue) {
-//                                 if ($subSubSubValue['route'] == Route::currentRouteName()) {
-// //                                     $this->menuList[$key]['route'][$subKey]['selected'] = true;
+                    if (is_array($subValue['route'])) {
+                        
+
+//                         foreach ($subValue as $subSubKey => $subSubValue) {
+//                             if (isset($subSubValue['route'])) {
+//                                 if ($subSubValue['route'] == Route::currentRouteName()) {
+//                                     $this->menuList[$key]['route'][$subSubKey]['selected'] = true;
+//                                 }
+//                             } else {
+//                                 foreach ($subSubValue as $subSubSubKey => $subSubSubValue) {
+//                                     if ($subSubSubValue['route'] == Route::currentRouteName()) {
+//                                         //                                     $this->menuList[$key]['route'][$subKey]['selected'] = true;
+//                                     }
 //                                 }
 //                             }
 //                         }
-//                     }
+                        
+                        
+                    } else {
+                        $this->menuList[$key]['route'][$subKey]['selected'] = true;
+                    }
+
                 }
             } else {
                 if ($value['route'] == Route::currentRouteName()) {
@@ -117,7 +115,7 @@ class Navigation extends Component
         }
         
         dd($this->menuList);
-        */
+        
         
         /*
         if (Auth::check()) {
