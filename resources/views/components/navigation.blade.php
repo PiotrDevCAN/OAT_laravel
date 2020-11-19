ab8617fe846fcf318d59e4bee6b60a21c4abf5c2
PiotrTajanowicz <PiotrTajanowicz@9.145.58.168>
2020-11-19 16:28:32 +0100

    upd

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -2,11 +2,35 @@
     <div class="ibm-parent" id="ibm-navigation">
          <ul aria-labelledby="ibm-pagetitle-h1" role="tree" id="ibm-primary-links">
             @foreach ($menuList as $key => $value)
-                @if (is_array($value['route']))
-                	
-                @else
-                	<li @if ($loop->first) id="ibm-overview" @endif role="presentation"><a href="{{ route($value['route']) }}" role="treeitem" @isset($value['selected']) aria-selected="true" @endisset>{{ $key }}</a></li>
-                @endif
+            	@isset($value['route'])
+                    @if (is_array($value['route']))
+                    	<li role="presentation">
+                        	<span class="ibm-subnav-heading">{{ $key }}</span>
+                        	<ul role="group">
+                        		@foreach ($value['route'] as $subKey => $subValue)
+                        			@isset($subValue['route'])
+                        				@if (is_array($subValue['route']))
+                            				<li role="presentation">
+                            					<span class="ibm-subnav-heading" style="padding-left: 10px;">{{ $subKey }}</span>
+                            					<ul role="group">
+                            						@foreach ($subValue['route'] as $subSubKey => $subSubValue)
+                            							@isset($subSubValue['route'])
+                            								<li role="presentation"><a href="{{ route($subSubValue['route']) }}" role="treeitem">{{ $subSubKey }}</a> </li>
+                            							@endisset
+                            						@endforeach
+                            					</ul>
+                            				</li>
+                            			@else
+                            				<li role="presentation"><a href="{{ route($subValue['route']) }}" role="treeitem">{{ $subKey }}</a></li>
+                            			@endif
+                        			@endisset
+                        		@endforeach
+                        	</ul>
+                        <li>                    	
+                    @else
+                    	<li @if ($loop->first) id="ibm-overview" @endif role="presentation"><a href="{{ route($value['route']) }}" role="treeitem" @isset($value['selected']) aria-selected="true" @endisset>{{ $key }}</a></li>
+                    @endif
+                @endisset
             @endforeach
 		</ul>
         <div id="ibm-secondary-navigation">
