d06122fc1be8cf03973ec1e79127e79f5b6f4015
PiotrTajanowicz <PiotrTajanowicz@9.145.58.168>
2020-11-20 16:17:35 +0100

    upd

--- resources/views/components/request/list.blade.php
+++ resources/views/components/request/list.blade.php
@@ -50,35 +50,34 @@ jQuery( document ).ready(function() {
 			            "url": "https://soiwapi-new.icds.ibm.com/OAT_laravel/api/request/list",
 			            "type": "POST",
 
-		            	"dataSrc": data,
 		                "datatype": "json",
 		                "contentType": "application/json; charset=utf-8"
 		                
 			        };
 					tabletDataObject.columns = [
-						{ "data": 'reference' },
-						{ "data": 'requestor' },
-						{ "data": 'requested' },
-						{ "data": 'competency' },
-						{ "data": 'approvaltype' },
-						{ "data": 'title' },
-						{ "data": 'account' },
-						{ "data": 'weekenddate' },
-						{ "data": 'nature' },
-						{ "data": 'details' },
-						{ "data": 'worker' },
-						{ "data": 'serial' },
-						{ "data": 'hours' },
-						{ "data": 'status' },
-						{ "data": 'rejection' },
-						{ "data": 'supercedes' },
-						{ "data": 'supercededby' },
-						{ "data": 'claim_acc_id' },
-						{ "data": 'approver_first_level' },
-						{ "data": 'approver_first_level_ts' },
-						{ "data": 'approver_second_level' },
-						{ "data": 'approver_second_level_ts' },
-						{ "data": 'approver_third_level' }
+						{ data: 'reference' },
+						{ data: 'requestor' },
+						{ data: 'requested' },
+						{ data: 'competency' },
+						{ data: 'approvaltype' },
+						{ data: 'title' },
+						{ data: 'account' },
+						{ data: 'weekenddate' },
+						{ data: 'nature' },
+						{ data: 'details' },
+						{ data: 'worker' },
+						{ data: 'serial' },
+						{ data: 'hours' },
+						{ data: 'status' },
+						{ data: 'rejection' },
+						{ data: 'supercedes' },
+						{ data: 'supercededby' },
+						{ data: 'claim_acc_id' },
+						{ data: 'approver_first_level' },
+						{ data: 'approver_first_level_ts' },
+						{ data: 'approver_second_level' },
+						{ data: 'approver_second_level_ts' },
+						{ data: 'approver_third_level' }
 			        ];
 					
 				    IBMCore.common.widget.datatable.init(tableData[n], tabletDataObject);
