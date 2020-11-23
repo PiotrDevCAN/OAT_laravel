f9cbcdea7cbdf8eb64a4d553f2d6398a0daa7edc
PiotrTajanowicz <PiotrTajanowicz@9.145.58.168>
2020-11-23 15:05:39 +0100

    upd

--- resources/views/components/request/list.blade.php
+++ resources/views/components/request/list.blade.php
@@ -34,64 +34,6 @@ jQuery( document ).ready(function() {
 			switch(tabletDataObject.widget){
 				case 'datatableReady':
 
-					var params = {
-						
-						status: tableData.eq(n).attr('id'),
-			        	scrollaxis: "x", 
-			        	info: true,
-			        	ordering: true, 
-			        	paging: true, 
-			        	searching: true,
-			        	
-				        processing: true,
-				        serverSide: true,
-
-				        ajax = {
-				            url: 'https://soiwapi-new.icds.ibm.com/OAT_laravel/api/request/list',
-				            type: 'POST',
-				            dataSrc: 'data',
-				            data: function ( d ) {
-				            	// awaiting
-				            	// approved
-				            	// other
-				            	d.type = "awaiting";
-				            }
-				        },
-				        
-				        deferLoading: 57,
-				        columns: [
-				        	{ "data": "reference" },
-							{ "data": "account" },
-							{ "data": "competency" },
-							{ "data": "nature" },
-							{ "data": "title" },
-							{ "data": "details" },
-							{ "data": "weekenddate" },
-							{ "data": "worker" },
-							{ "data": "serial" },
-							{ "data": "location" },
-							{ "data": "hours" },
-							{ "data": "status" },
-							{ "data": "approver_first_level" },
-							{ "data": "approver_second_level" },
-							{ "data": "approver_third_level" },
-							{ "data": "requestor" },
-							{ "data": "approval_mode" },
-							{ "data": "approver_squad_leader" },
-							{ "data": "approver_tribe_leader" },
-							{ "data": "supercedes" },
-							{ "data": "supercededby" },
-							{ "data": "claim_acc_id" },
-							{ "data": "created_ts" }
-				        ]
-				    }
-					
-					// additional settings
-// 					tabletDataObject.pageLength = -1;
-// 					tabletDataObject.lengthMenu = [[10, 25, 50, -1], [10, 25, 50, "All"]];
-
-				    IBMCore.common.widget.datatable.init(tableData[n], params);
-				    
 					break;
 				default:
 					break;
