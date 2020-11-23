72e7a5a095c07af4b99e63a1cf9d8935f6a26e05
PiotrTajanowicz <PiotrTajanowicz@9.145.159.221>
2020-11-23 15:38:23 +0100

    upd

--- resources/views/components/request/list.blade.php
+++ resources/views/components/request/list.blade.php
@@ -25,19 +25,73 @@ jQuery( document ).ready(function() {
 	
 	if (typeof (tableData) !== 'undefined') {
 		for (n=0;n<tableData.length;n++){
-
-			console.log(tableData.eq(n).attr("id"));
-
-//			console.log(tableData.eq(n));
 			
 			// default object settings
 			var tabletDataObject = tableData.eq(n).data();
 
-			alert(tabletDataObject.widget);
-			
 			switch(tabletDataObject.widget){
 				case 'datatableReady':
 
+					console.log(params);
+					
+					var params = {
+						
+						status: tableData.eq(n).attr('id'),
+			        	scrollaxis: "x", 
+			        	info: true,
+			        	ordering: true, 
+			        	paging: true, 
+			        	searching: true,
+			        	
+// 				        processing: true,
+				        serverSide: true,
+
+				        ajax: {
+				            url: "https://soiwapi-new.icds.ibm.com/OAT_laravel/api/request/list",
+				            type: "POST",
+				            dataSrc: "data",
+				            data: function ( d ) {
+				            	// awaiting
+				            	// approved
+				            	// other
+				            	d.type = "awaiting";
+				            }
+				        },
+				        
+				        deferLoading: 57,
+				        columns: [
+				        	{ "data": "reference" },
+							{ "data": "account" },
+							{ "data": "competency" },
+							{ "data": "nature" },
+							{ "data": "title" },
+							{ "data": "details" },
+							{ "data": "weekenddate" },
+							{ "data": "worker" },
+							{ "data": "serial" },
+							{ "data": "location" },
+							{ "data": "hours" },
+							{ "data": "status" },
+							{ "data": "approver_first_level" },
+							{ "data": "approver_second_level" },
+							{ "data": "approver_third_level" },
+							{ "data": "requestor" },
+							{ "data": "approval_mode" },
+							{ "data": "approver_squad_leader" },
+							{ "data": "approver_tribe_leader" },
+							{ "data": "supercedes" },
+							{ "data": "supercededby" },
+							{ "data": "claim_acc_id" },
+							{ "data": "created_ts" }
+				        ]
+				    }
+					
+					// additional settings
+// 					tabletDataObject.pageLength = -1;
+// 					tabletDataObject.lengthMenu = [[10, 25, 50, -1], [10, 25, 50, "All"]];
+
+				    IBMCore.common.widget.datatable.init(tableData[n], params);
+				    
 					break;
 				default:
 					break;
