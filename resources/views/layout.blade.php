9bf2e3176d262dc4b609bf42604292160ea93e74
PiotrTajanowicz <PiotrTajanowicz@9.145.159.221>
2020-11-24 12:33:09 +0100

    upd

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,27 +19,28 @@
         
     	<script> 
 		digitalData = {
-            "page":{
-                "category":{
-                	"primaryCategory":"REPLACE"
+            page: {
+                category: {
+                	primaryCategory: "SB03"
                 },
-                "pageInfo":{
-                    "effectiveDate":"REPLACE",
-                    "expiryDate":"REPLACE",
-                    "language":"en-UK",
-                    "publishDate":"REPLACE",
-                    "publisher":"IBM Corporation",
-                    "version":"v18",
-                    "pageID":"REPLACE",
-                    "ibm":{
-                        "contentDelivery":"HTML",
-                        "contentProducer":"IBM Northstar Template Generator 2.0",
-                        "country":"UK",
-                        "industry":"REPLACE",
-                        "owner":"REPLACE",
-                        "siteID":"REPLACE",
-                        "subject":"REPLACE",
-                        "type":"REPLACE"
+                pageInfo: {
+                	author: "Piotr Tajanowicz",
+                    effectiveDate: "2014-11-19",
+                    expiryDate: "2030-11-19",
+                    language: "en-US",
+                    publishDate: "2014-11-19",
+                    publisher: "IBM Corporation",
+                    version: "v18",
+                    ibm: {
+                        contentDelivery: "HTML",
+                        contentProducer: "IBM Northstar Template Generator 2.0",
+                        country: "UK",
+                        industry: "______",
+                        owner: "Piotr Tajanowicz/Poland/IBM",
+                        owningPortal: "______",
+                        siteID: "______",
+                        subject: "______",
+                        type: "CT###"
                     }
                 }
             }
