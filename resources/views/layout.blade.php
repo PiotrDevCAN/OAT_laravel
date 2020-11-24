1dc58e747b8c11bf94eefde3ed22e183c2c334d4
PiotrTajanowicz <PiotrTajanowicz@9.145.159.221>
2020-11-24 12:48:04 +0100

    upd

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,34 +17,34 @@
         <meta name="generator" content="IBM Northstar Template Generator 2.0" />
         <title>{{ config('app.name') }}</title>
         
-    	<script> 
-		digitalData = {
-            page: {
-                category: {
-                	primaryCategory: "SB03"
-                },
-                pageInfo: {
-                	author: "Piotr Tajanowicz",
-                    effectiveDate: "2014-11-19",
-                    expiryDate: "2030-11-19",
-                    language: "en-US",
-                    publishDate: "2014-11-19",
-                    publisher: "IBM Corporation",
-                    version: "v18",
-                    ibm: {
-                        contentDelivery: "HTML",
-                        contentProducer: "IBM Northstar Template Generator 2.0",
-                        country: "UK",
-                        industry: "______",
-                        owner: "Piotr Tajanowicz/Poland/IBM",
-                        owningPortal: "______",
-                        siteID: "______",
-                        subject: "______",
-                        type: "CT###"
+    	<script type="text/javascript">
+    		digitalData = {
+            	page: {
+                    category: {
+                    	primaryCategory: "SB03"
+                    },
+                    pageInfo: {
+                    	author: "Piotr Tajanowicz",
+                        effectiveDate: "2014-11-19",
+                        expiryDate: "2030-11-19",
+                        language: "en-US",
+                        publishDate: "2014-11-19",
+                        publisher: "IBM Corporation",
+                        version: "v18",
+                        ibm: {
+                            contentDelivery: "HTML",
+                            contentProducer: "IBM Northstar Template Generator 2.0",
+                            country: "UK",
+                            industry: "______",
+                            owner: "Piotr Tajanowicz/Poland/IBM",
+                            owningPortal: "______",
+                            siteID: "______",
+                            subject: "______",
+                            type: "CT###"
+                        }
                     }
                 }
-            }
-    	}; 
+        	}; 
     	</script>
         <script src="//1.www.s81c.com/common/stats/ida_stats.js"></script>
         <link href="//1.www.s81c.com/common/v18/css/www.css" rel="stylesheet" />
