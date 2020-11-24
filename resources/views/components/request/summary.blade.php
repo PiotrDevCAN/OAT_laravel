1317af700e304a784eca578829d84cd20cfb9af3
PiotrTajanowicz <PiotrTajanowicz@9.145.159.221>
2020-11-24 12:13:06 +0100

    upd

--- resources/views/components/request/summary.blade.php
+++ resources/views/components/request/summary.blade.php
@@ -8,24 +8,24 @@
         </div>
         <div class="ibm-fluid">
         	<div class="ibm-col-12-6">
-            	<x-ibmv18form-input field-name="AwaitingApprovalRequests" label="Awaiting Approval Requests:" :value="{{ $awaitingTotal ?? 0 }}" disabled="true"/>
+            	<x-ibmv18form-input field-name="AwaitingApprovalRequests" label="Awaiting Approval Requests:" :value="{{ $awaitingTotal ?? '0' }}" disabled="true"/>
             </div>
             <div class="ibm-col-12-6">
-            	<x-ibmv18form-input field-name="ApprovedRequestsHours" label="Hours:" :value="{{ $awaitingHours ?? 0 }}" disabled="true"/>
+            	<x-ibmv18form-input field-name="ApprovedRequestsHours" label="Hours:" :value="{{ $awaitingHours ?? '0' }}" disabled="true"/>
             </div>
         	
         	<div class="ibm-col-12-6">
-            	<x-ibmv18form-input field-name="ApprovedRequests" label="Approved Requests:" :value="{{ $approvedTotal ?? 0 }}" disabled="true"/>
+            	<x-ibmv18form-input field-name="ApprovedRequests" label="Approved Requests:" :value="{{ $approvedTotal ?? '0' }}" disabled="true"/>
             </div>
             <div class="ibm-col-12-6">
             	<x-ibmv18form-input field-name="AwaitingApprovalRequestsHours" label="Hours:" :value="{{ $approvedHours ?? 0 }}" disabled="true"/>
             </div>
         	
             <div class="ibm-col-12-6">
-            	<x-ibmv18form-input field-name="OtherRequests" label="Other Requests:" :value="{{ $otherTotal ?? 0 }}" disabled="true"/>
+            	<x-ibmv18form-input field-name="OtherRequests" label="Other Requests:" :value="{{ $otherTotal ?? '0' }}" disabled="true"/>
             </div>
             <div class="ibm-col-12-6">
-            	<x-ibmv18form-input field-name="OtherRequestsHours" label="Hours:" :value="{{ $otherHours ?? 0 }}" disabled="true"/>
+            	<x-ibmv18form-input field-name="OtherRequestsHours" label="Hours:" :value="{{ $otherHours ?? '0' }}" disabled="true"/>
             </div>
        	</div>
     </div>
