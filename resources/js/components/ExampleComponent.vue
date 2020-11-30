28ba458785a724b265625462dff53ccdfdbb9366
PiotrTajanowicz <PiotrTajanowicz@9.145.159.221>
2020-11-30 15:33:44 +0100

    upd

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -7,6 +7,9 @@
 
                     <div class="card-body">
                         I'm an example component.
+                        
+                        <router-view></router-view>
+                        
                     </div>
                 </div>
             </div>
