921a868556cdbaefc653e56162b7497c4a367fa4
PiotrTajanowicz <PiotrTajanowicz@9.145.159.221>
2020-11-30 17:40:30 +0100

    upd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,9 +11,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{config('app.url')}}:6001/socket.io/socket.io.js"></script>
-    
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
