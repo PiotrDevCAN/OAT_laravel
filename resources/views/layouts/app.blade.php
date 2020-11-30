43f1b9da02312045de7dda0fd345419fe5c64269
PiotrTajanowicz <PiotrTajanowicz@9.145.159.221>
2020-11-30 13:17:37 +0100

    upd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,17 +12,7 @@
 
     <!-- Scripts -->
     <script src="{{config('app.url')}}:6001/socket.io/socket.io.js"></script>
-    <script>
-        @auth()
-        window.appConfig = {
-            auth:@json(Auth::user()->toCredentials())
-        };
-        @elseauth()
-            window.appConfig = {
-            auth:null
-        };
-        @endauth
-    </script>
+    
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
