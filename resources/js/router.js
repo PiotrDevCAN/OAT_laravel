1ed4b005acfe377d21f5f7a55d3f940f04214148
PiotrTajanowicz <PiotrTajanowicz@9.145.159.221>
2020-12-01 10:57:22 +0100

    upd

--- resources/js/router.js
+++ resources/js/router.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import Router from 'vue-router'
 Vue.use(Router)
 import firstPage from './components/pages/myFirstVuePage'
-const routes = [
+const routes = [	
+	{
+		path: '/',
+		component: firstPage,
+	},
+	{
+		path: '/vue',
+		component: firstPage,
+	},
 	{
 		path: '/my-new-vue-route',
 		name: firstPage,
