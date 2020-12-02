14ff910c305fe5f098af1cc63f583f25188fc0e9
PiotrTajanowicz <PiotrTajanowicz@9.211.142.244>
2020-12-02 11:11:00 +0100

    upd

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
-
 import 'carbon-components/scripts/carbon-components.js';
-
 //import myDefault from 'carbon-components/scripts/carbon-components.js';
+
+var Modal = CarbonComponents.Modal;
+console.log(Modal);
