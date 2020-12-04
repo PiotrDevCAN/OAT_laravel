d0e892e67a34311b60602b0e7e5e3ef02b5ebd5c
PiotrTajanowicz <PiotrTajanowicz@9.211.142.244>
2020-12-04 15:50:58 +0100

    upd

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,14 @@ import CarbonComponents from 'carbon-components/scripts/carbon-components.js';
 var Modal = CarbonComponents.Modal;
 console.log(Modal);
 
+console.log('CarbonComponents');
 console.log(CarbonComponents);
 
-console.log(CarbonComponents.settings.disableAutoInit);
+var settings = CarbonComponents.settings;
+console.log('settings');
+console.log(settings);
+
+console.log('settings disableAutoInit');
+console.log(settings.disableAutoInit);
 
 alert('modal component');
