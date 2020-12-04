7b894c8f8dddf8275dc2c73c3b1274b77d83704c
PiotrTajanowicz <PiotrTajanowicz@9.211.142.244>
2020-12-04 17:19:10 +0100

    upd

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,12 +20,7 @@ alert('modal component');
 import 'carbon-web-components/es/components/dropdown/dropdown';
 import 'carbon-web-components/es/components/dropdown/dropdown-item';
 
-import 'carbon-web-components/es/components/ui-shell/header-nav';
-import 'carbon-web-components/es/components/ui-shell/header-submenu';
-import 'carbon-web-components/es/components/ui-shell/navigation-menu';
-import 'carbon-web-components/es/components/ui-shell/navigation-menu-panel';
-import 'carbon-web-components/es/components/ui-shell/product-switcher';
-import 'carbon-web-components/es/components/ui-shell/side-nav';
+import 'carbon-web-components/es/index';
 
 
-alert('this should work');
+alert('this should work !!!');
